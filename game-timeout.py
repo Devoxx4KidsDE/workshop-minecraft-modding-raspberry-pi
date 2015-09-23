b673624c72d2a498882908a487625c15ef4d01f6
greenhalos <greenhalos@gmail.com>
2015-09-23 16:41:14 +0200

    add correct whitespaces

--- game-timeout.py
+++ game-timeout.py
@@ -7,14 +7,14 @@ y = mc.getHeight(0,0)
 
 mc.postToChat("Begin")
 mc.setting("world_immutable", True)
-mc.setBlock(-5,y-1,5,block.GLOWING_OBSIDIAN)
-mc.setBlock( -5, y, 5,block.GOLD_ORE)
-mc.setBlock( -5, y, -5,block.GOLD_ORE)
-mc.setBlock( 5, y, 5,block.GOLD_ORE)
-mc.setBlock( 5, y, -5,block.GOLD_ORE)
-mc.setBlock(5,y - 1,-5,block.GOLD_ORE)
+mc.setBlock(-5, y-1, 5, block.GLOWING_OBSIDIAN)
+mc.setBlock( -5, y, 5, block.GOLD_ORE)
+mc.setBlock( -5, y, -5, block.GOLD_ORE)
+mc.setBlock( 5, y, 5, block.GOLD_ORE)
+mc.setBlock( 5, y, -5, block.GOLD_ORE)
+mc.setBlock(5, y - 1, -5, block.GOLD_ORE)
 
-mc.player.setTilePos(0,y,0)
+mc.player.setTilePos(0, y, 0)
 points = 0
 timer = 5
 while True:
@@ -23,15 +23,15 @@ while True:
         hitBlock = mc.getBlock(event.pos.x, event.pos.y, event.pos.z)
         if hitBlock == block.GOLD_ORE.id:
             points += 1
-            mc.setBlock(event.pos.x , event.pos.y , event.pos.z , block.AIR.id)
+            mc.setBlock(event.pos.x, event.pos.y, event.pos.z, block.AIR.id)
             timer += 3
         if hitBlock == block.GLOWING_OBSIDIAN.id:
             points += 3
-            mc.setBlock(event.pos.x , event.pos.y , event.pos.z , block.GRASS.id)
+            mc.setBlock(event.pos.x, event.pos.y, event.pos.z, block.GRASS.id)
             timer += 5
         if points == 7:
-           mc.postToChat("Du hast gewonnen in " + str(timer) + " Sekunden")
-           break
+            mc.postToChat("Du hast gewonnen in " + str(timer) + " Sekunden")
+            break
     timer -= 1
     print timer
     time.sleep(1)
